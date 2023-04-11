275fdb1ffecc0ed44cec9c729e63d412ff6be919
sibgatullin-i <67420207+sibgatullin-i@users.noreply.github.com>
2023-04-11 09:27:11 +0300

    Update EDI_parser.ps1
    ignore ssl errors when downloading

--- EDI_parser.ps1
+++ EDI_parser.ps1
@@ -76,6 +76,23 @@ $pop3Client.dispose()
 $incomingFiles = (Get-ChildItem $settings.inboxFolder)
 $msg += "`r`nfiles saved: $((Get-ChildItem $settings.inboxFolder).count)"
 
+### ignore ssl errors ###
+add-type @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+    public bool CheckValidationResult(
+        ServicePoint srvPoint, X509Certificate certificate,
+        WebRequest request, int certificateProblem) {
+        return true;
+    }
+}
+"@
+$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+#########################
+
 # proceed files - move, parse, download and shit
 foreach ($incomingFile in $incomingFiles) {
   $incomingFileBaseName = ($incomingFile.BaseName -split "-_-")[1]
