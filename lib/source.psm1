54b2d8cb40e2485b6be0ac94382184a7601708b9
sibgatullin-i <67420207+sibgatullin-i@users.noreply.github.com>
2023-09-14 11:19:41 +0300

    back IDs

--- lib/source.psm1
+++ lib/source.psm1
@@ -109,7 +109,7 @@ function Download-Pages {
     $newPath = Join-Path -Path $Folder -ChildPath $newName
     $currentItemPosition += 1
     $currentProgress = [Math]::Round(($currentItemPosition * 100) / $sourceData.Count)
-    Write-Progress -Activity "Downloading" -Status "$currentItemPosition / $($sourceData.Count): $newName..." -PercentComplete $currentProgress
+    Write-Progress -ParentId $ProgressParentId -Id 101 -Activity "Downloading" -Status "$currentItemPosition / $($sourceData.Count): $newName..." -PercentComplete $currentProgress
     write-host "Downloading $newName..."
     try { $page = (Invoke-WebRequest -UseBasicParsing $item.Url).Content }
     catch { write-warning "$newName download failed" }
